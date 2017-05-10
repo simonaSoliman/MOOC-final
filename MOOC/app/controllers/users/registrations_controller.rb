1a72d985f2b4573b49bb26af6fc91dde1c0372da
simonaSoliman <simona.soliman@yahoo.com>
2017-05-11 01:22:23 +0200

    try to solve saving in database

--- MOOC/app/controllers/users/registrations_controller.rb
+++ MOOC/app/controllers/users/registrations_controller.rb
@@ -39,14 +39,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :DOB, :gender, :image, :password, :password_confirmation])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :DOB, :gender, :image, :password, :password_confirmation, :current_password])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
